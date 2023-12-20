45d4ac85a755d6dc18a2eef90a9fa9a36c068343
MatthewMMontoya <montoyam.matthew@gmail.com>
2023-12-20 15:24:53 -0700

    src/main/java/com/atomicaggies/gui/avionicsgui/DataAcquisitionService.java

---
+++ src/main/java/com/atomicaggies/gui/avionicsgui/DataAcquisitionService.java
@@ -0,0 +1,15 @@
+package com.atomicaggies.gui.avionicsgui;
+
+public class DataAcquisitionService {
+    private TelemetryDataModel telemetryDataModel;
+
+    public DataAcquisitionService(TelemetryDataModel telemetryDataModel) {
+        this.telemetryDataModel = telemetryDataModel;
+    }
+
+    public void start() {
+        // Implement data acquisition logic
+        // ...
+    }
+}
+
