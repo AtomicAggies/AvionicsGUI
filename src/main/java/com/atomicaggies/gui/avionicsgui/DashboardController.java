7b1d0533dad5dc82dfd401dd2f478fc558ea71e7
MatthewMMontoya <montoyam.matthew@gmail.com>
2023-12-20 15:23:53 -0700

    DashController class

--- src/main/java/com/atomicaggies/gui/avionicsgui/DashboardController.java
+++ src/main/java/com/atomicaggies/gui/avionicsgui/DashboardController.java
@@ -9,10 +9,25 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable;
 
 public class DashboardController {
     @FXML
-    private Label welcomeText;
+    private Label welcomeText;  // FIXME temporary visual
+
+    private CompositeDisposable disposables = new CompositeDisposable();
+    private TelemetryDataModel telemetryDataModel = new TelemetryDataModel();
+    private Tile temperatureTile; // Add UI element for temperature
+    private Tile humidityTile;    // Add UI element for humidity
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }   //FIXME remove button
+
+
+    @FXML
+    public void initialize() {
+        // Initialize Tiles and bind them to model
+        // ...
     }
+
+    // Add methods for binding and updating tiles
+    // ...
 }
