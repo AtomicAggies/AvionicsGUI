5e04b8539b92737fb12242cf8c67df62bd9e8232
MatthewMMontoya <montoyam.matthew@gmail.com>
2023-12-20 15:11:53 -0700

    tracking TelemetryDataModel.java

---
+++ src/main/java/com/atomicaggies/gui/avionicsgui/TelemetryDataModel.java
@@ -0,0 +1,26 @@
+package com.atomicaggies.gui.avionicsgui;
+
+import io.reactivex.rxjava3.subjects.BehaviorSubject;
+import io.reactivex.rxjava3.core.Observable;
+
+public class TelemetryDataModel {
+    private BehaviorSubject<Double> temperatureSubject = BehaviorSubject.create();
+    private BehaviorSubject<Double> humiditySubject = BehaviorSubject.create();
+
+    public Observable<Double> getTemperatureObservable() {
+        return temperatureSubject.hide();
+    }
+
+    public Observable<Double> getHumidityObservable() {
+        return humiditySubject.hide();
+    }
+
+    public void updateTemperature(double newTemperature) {
+        temperatureSubject.onNext(newTemperature);
+    }
+
+    public void updateHumidity(double newHumidity) {
+        humiditySubject.onNext(newHumidity);
+    }
+}
+
