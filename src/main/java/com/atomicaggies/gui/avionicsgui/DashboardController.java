bbcd534eed70117e8e92700f3a5b31bfccedbd48
MatthewMMontoya <montoyam.matthew@gmail.com>
2023-12-20 15:17:52 -0700

    refactoring hello to dashboard

--- src/main/java/com/atomicaggies/gui/avionicsgui/DashboardController.java
+++ src/main/java/com/atomicaggies/gui/avionicsgui/DashboardController.java
@@ -1,9 +1,13 @@
 package com.atomicaggies.gui.avionicsgui;
 
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import eu.hansolo.tilesfx.Tile;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 
-public class HelloController {
+public class DashboardController {
     @FXML
     private Label welcomeText;
 
